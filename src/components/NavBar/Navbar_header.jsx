ee20732eb23617816b349a360e7ae22b57edba10
Максим <110373927+Maksim20000@users.noreply.github.com>
2022-12-28 14:54:51 +1000

    proverochkaDubledba

--- src/components/NavBar/Navbar_header.jsx
+++ src/components/NavBar/Navbar_header.jsx
@@ -11,7 +11,7 @@ class Navbar_header extends React.Component{
                         <NavLink to='/profile'>Profile</NavLink>
                     </div>
                     <div className={`${s.nav} ${s.item}`}>
-                        <NavLink to='/dialogs'>Message galjksagh;lajkshsdfkl</NavLink>
+                        <NavLink to='/dialogs'>Message gsm'lngsjla;hgd;os</NavLink>
                     </div>
                     <div className={s.item}>
                         <NavLink to='/new'>New</NavLink>
